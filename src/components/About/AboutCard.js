0b4e7a7949fb2b5039415d9889fa9f6e36235df9
Vinay <vinay.u.info@gmail.com>
2025-07-16 19:49:58 +0530

    Version 1

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Vinay Unagar </span>
+            from <span className="purple"> Surat, India.</span>
+            <br />
+            React.js & Frontend Developer with hands-on experience building scalable, responsive web appsusing React.js
+            <br />
+            Tailwind CSS, and REST APIs. Passionate about clean UI, performance, andmodern frameworks like Next.js
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/About/Techstack.js
@@ -0,0 +1,41 @@
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiGit,
+
+} from "react-icons/di";
+import {
+  SiFirebase,
+  SiNextdotjs,
+  SiPostgresql,
+} from "react-icons/si";
+
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+ 
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
